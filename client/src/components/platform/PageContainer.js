71805b6089b07c3f0da57132d25fcf48b80d7fb4
Stephen McLean <smclean@guidewire.com>
2018-09-12 14:00:41 +0100

    Small styling change

--- client/src/components/platform/PageContainer.js
+++ client/src/components/platform/PageContainer.js
@@ -4,12 +4,12 @@ export default styled.section`
     @media only screen and (min-width: 992px) {
         margin-left: 15rem; 
         margin-right: 15rem;
+        margin-top: 2rem; 
     }
     
     @media only screen and (max-width: 991px) {
         margin-left: 1rem; 
         margin-right: 1rem;
+        margin-top: 1rem; 
     }
-    
-    margin-top: 2rem; 
 `;
