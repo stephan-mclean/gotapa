745ba91cedd18bf23e1f390c70129eaae7ea9e30
Stephen McLean <smclean@guidewire.com>
2018-09-12 14:22:16 +0100

    Setup for deployment to heroku

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express(); 
+const path = require('path');
 const morgan = require('morgan');
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
 
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI);
-mongoose.connection.on('error', console.error.bind(console, 'DB connection error'));
+mongoose.connection.on('error', console.error.bind(console, 'DB connection error')); */ 
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const api = require('./server/routes/api');
 app.use('/api', api);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(process.env.PORT || 5000, () => console.log('Server started'));
