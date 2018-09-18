36cde6308df494edda65f733eadfa0356c69b58d
Stephen McLean <stephenmclean@Taras-MacBook-Pro.local>
2018-09-18 19:52:55 +0100

    Update the nearby locations to always show from user input, rather than trying to show if permission was previously given.

--- client/src/components/NearbyLocations/NearbyLocations.js
+++ client/src/components/NearbyLocations/NearbyLocations.js
@@ -9,6 +9,9 @@ import IconMessage from '../IconMessage/IconMessage';
 import InfoMessage from '../InfoMessage/InfoMessage';
 import Button from '../platform/Button';
 
+const POSITION_UNAVAILABLE = 2; 
+const POSITION_TIMEOUT = 3; 
+
 const TitleContainer = styled.label`
     display: block; 
     font-weight: bold; 
@@ -21,36 +24,31 @@ const LocationsRenderBy = ({ ...otherProps, item }) => {
     );
 };
 
-// TODO: Deal with if user declines permission. 
-// TODO: Deal with if position can't be found. 
 class NearbyLocations extends React.Component {
 
     constructor(props) {
         super(props);
 
-        this.state = { locations: [], loading: false, geolocationAllowed: false, geolocationSupported: !!navigator.geolocation };
+        this.state = { 
+            locations: [], 
+            loading: false,
+            error: false, 
+            geolocationAllowed: false, 
+            geolocationSupported: !!navigator.geolocation 
+        };
+
         this.tryToEnableLocation = this.tryToEnableLocation.bind(this);
         this.onLocationClicked = this.onLocationClicked.bind(this);
     }
 
-    componentDidMount() {
-
-        if(this.state.geolocationSupported) {
-            if (navigator.permissions) {
-                navigator.permissions.query({ name: 'geolocation'})
-                    .then(permission => {
-                        if (permission.state === 'granted') {
-                            this.tryToEnableLocation(); 
-                        }
-                    });
-            }
-        }
-
-    }
-
     tryToEnableLocation() {
+        const options = {
+            timeout: 5000
+        };
+
+        this.setState({ loading: true });
         navigator.geolocation.getCurrentPosition(position => {
-            this.setState({ geolocationAllowed: true, loading: true });
+            this.setState({ geolocationAllowed: true});
             const { latitude, longitude } = position.coords; 
             fetch(`/api/search/nearby?latitude=${latitude}&longitude=${longitude}&limit=5`)
                 .then(response => response.json())
@@ -58,7 +56,11 @@ class NearbyLocations extends React.Component {
                     const mappedLocations = data.map(stop => new StopModel(stop));
                     this.setState({ locations: mappedLocations, loading: false });
                 });
-        });
+        }, error => {
+            if (error.code === POSITION_UNAVAILABLE || error.code === POSITION_TIMEOUT) {
+                this.setState({ loading: false, error: true });
+            }
+        }, options);
     }
 
     onLocationClicked(item) {
@@ -72,21 +74,26 @@ class NearbyLocations extends React.Component {
         }
 
         const title = <TitleContainer>Nearby Locations</TitleContainer>;
-        if (!this.state.geolocationAllowed) {
-            return (
-                <div>
-                    {title}
-                    <InfoMessage>You need to <Button onClick={this.tryToEnableLocation} link>enable location services</Button> to show nearby locations.</InfoMessage>
-                </div>
-            );
-        } else if (this.state.loading) {
+
+        if (this.state.loading) {
             return (
                 <div>
                     {title}
                     <Loading message="Loading nearby locations..." />
                 </div>
             );
-        } else if(this.state.locations && this.state.locations.length) {
+        } else if (!this.state.geolocationAllowed) {
+            return (
+                <div>
+                    {title}
+                    <InfoMessage>
+                        <Button onClick={this.tryToEnableLocation} link>
+                            Click here
+                        </Button> to show nearby locations.
+                    </InfoMessage>
+                </div>
+            );
+        } else if (this.state.locations && this.state.locations.length) {
             return (
                 <div>
                     {title}
@@ -95,6 +102,13 @@ class NearbyLocations extends React.Component {
                           useKey="stopId" onItemClick={this.onLocationClicked} />
                 </div>
             );
+        } else if (this.state.error) {
+            return (
+                <div>
+                    {title}
+                    <IconMessage icon="exclamation-triangle" message="Something went wrong loading nearby locations..." />
+                </div>
+            ); 
         } else {
             return (
                 <div>
