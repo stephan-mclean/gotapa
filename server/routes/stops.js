8819605127c9d7f1163f27c7676d59fb380bdb35
Stephen McLean <smclean@guidewire.com>
2018-09-19 09:41:16 +0100

    Remove caching for single stops since we cache the entire list anyway.

--- server/routes/stops.js
+++ server/routes/stops.js
@@ -5,7 +5,7 @@ const cache = require('../utils/cache').cache;
 const stopsController = require('../controllers/stops');
 const asyncMiddleware = require('../utils/asyncMiddleware');
 
-router.get('/:id', cache('1 day'), asyncMiddleware(stopsController.getStop));
+router.get('/:id', asyncMiddleware(stopsController.getStop));
 router.get('/:id/realtimeinfo', cache('1 minutes'), asyncMiddleware(stopsController.getStopRealtimeInfo));
 router.post('/:id/addrecentlyviewed', stopsController.addRecentlyViewedStop);
 
