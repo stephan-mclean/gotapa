9178be8ceea0ae84a0879493a8bd3997030261f7
Stephen McLean <smclean@guidewire.com>
2018-09-20 11:01:34 +0100

    Update the select component

--- client/src/components/platform/Select.js
+++ client/src/components/platform/Select.js
@@ -1,119 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import List from './List';
-
-const Container = styled.div`
-    outline: none; 
-    display: flex; 
-    flex-direction: ${props => props.inline ? 'row' : 'column'};
-    align-items: ${props => props.inline ? 'center' : 'default'};
-`;
-
-const StyledInputContainer = styled.div` 
-    margin-bottom: 0.5rem;
-    width: ${props => !props.inline ? '100%' : 'auto'}; 
-    display: ${props => props.inline ? 'inline-block' : 'block'};
-    position: relative; 
-    background: ${props => props.theme.background};
-`;
-
-const StyledSelect = styled.div`
-    color: ${props => props.theme.secondary};
-    padding: 0.5rem;
-    border: ${props => `1px solid ${props.theme.tertiary}`}; 
-    border-radius: 5px; 
-`;
 
 const Label = styled.label`
-    margin-right: 0.5rem;  
+    margin-right: 1rem;  
     display: ${props => props.inline ? 'inline' : 'block'};
     margin-bottom: 0.5rem; 
     font-weight: bold; 
 `;
 
-const StyledList = styled(List)`
-    position: absolute;
-    width: 100%; 
-    background: ${props => props.theme.background};
-    margin-top: 0; 
-    border-top: none; 
-    z-index: 100; 
+const StyledSelect = styled.select`
+    padding: 0.5rem;
+    border: ${props => `1px solid ${props.theme.tertiary}`}; 
+    border-radius: 5px; 
+    outline: none; 
+    flex: 1 0 auto;
+    text-align: left; 
+    font-size: 1rem; 
+    color: ${props => props.theme.secondary};
+
+    :focus {
+        border: ${props => `1px solid ${props.theme.secondary}`}; 
+    }
+`;
+
+const StyledInputContainer = styled.div`
+    display: inline-flex; 
+    position: relative; 
+    margin-bottom: 0.5rem;
+    width: ${props => !props.inline ? '100%' : 'auto'}; 
 `;
 
 class Select extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = { value: props.value, placeholder: this.props.placeholder || 'Select', showOptions: false };
-
-        this.onFocus = this.onFocus.bind(this);
-        this.onBlur = this.onBlur.bind(this);
-    }
-
-    onChange() {
-        this.setState({ value: 'value' });
-    }
-
-    onFocus() {
-        console.log('select focused');
-
-        this.setState({ showOptions: true });
-    }
-
-    onBlur() {
-        console.log('select blurred');
-        this.setState({ showOptions: false });
     }
 
     render() {
 
-        const { inline, children, ...withoutChildren } = this.props; 
+        const { inline } = this.props; 
 
         let label; 
         if (this.props.label) {
             label = <Label htmlFor={this.props.id} {...{ inline }}>{this.props.label}</Label>
         }
 
-        let options; 
-
-        if (this.state.showOptions) {
-            if (this.props.renderOptionsBy) {
-
-                const customListItem = props => (
-                    <li {...props}>{props.item[this.props.renderOptionsBy]}</li>
-                );
-    
-                options = <StyledList items={this.props.options} renderBy={customListItem}></StyledList>
-    
-            } else {
-                console.log('regular options');
-                options = <StyledList items={this.props.options} />
-            }
-        }
-
+        const { children, ...withoutChildren } = this.props; 
         return (
-            <Container inline={inline} tabIndex="1" onFocus={this.onFocus} onBlur={this.onBlur}>
+            <div>
                 {label}
                 <StyledInputContainer inline={inline}>
-                    
-                    <StyledSelect {...withoutChildren}>
-                        {this.state.value || this.state.placeholder}
-                    </StyledSelect>
-
-                    {options}
+                    <StyledSelect {...withoutChildren}>{children}</StyledSelect>
                 </StyledInputContainer>
-            </Container>
+            </div>
         );
+
     }
 
 };
-
-Select.propTypes = {
-    renderOptionsBy: PropTypes.string,
-    options: PropTypes.array
-};
-
 
 export default Select; 
--- client/src/components/platform/Select.stories.js
+++ client/src/components/platform/Select.stories.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { State, Store } from '@sambego/storybook-state';
 import { ThemeProvider } from 'styled-components';
 import Themes from '../../utils/ThemeUtil'; 
 import Select from './Select';
 
+const store = new Store({
+    selectOneVal: '',
+    selectTwoVal: '',
+    selectThreeVal: ''
+});
+
 const stories = storiesOf('Components', module);
 
-stories.add('Select', () => (
-    <ThemeProvider theme={Themes.main}>
-        <div>
-            <Select options={[1, 2, 3]} />
+stories.add('Select', () => {
 
-            <Select label="With Label" options={[4, 5, 6]} />
+    const onChange = (id, e) => {
+        store.set({
+            id : e.target.value
+        });
+    };
 
-            <Select label="With Inline Label" inline options={[7, 8, 9]} />
+    return (
+        <ThemeProvider theme={Themes.main}>
+            <State store={store}>
+                <Select value={store.selectOneVal} onChange={onChange.bind(null, 'selectOneVal')}>
+                    <option value="">Select</option>
+                    <option value="One">One</option>
+                    <option value="Two">Two</option>
+                    <option value="Three">Three</option>
+                </Select>
+    
+                <Select value={store.selectTwoVal} onChange={onChange.bind(null, 'selectTwoVal')} label="With Label">
+                    <option value="">Select</option>
+                    <option value="One">One</option>
+                    <option value="Two">Two</option>
+                    <option value="Three">Three</option>
+                </Select>
+    
+                <Select value={store.selectThreeVal} onChange={onChange.bind(null, 'selectThreeVal')} label="With Inline Label" inline>
+                    <option value="">Select</option>
+                    <option value="One">One</option>
+                    <option value="Two">Two</option>
+                    <option value="Three">Three</option>
+                </Select>
+            </State>
+        </ThemeProvider>
+    );
 
-            <Select label="With Render By" 
-                    options={[{ name: 'ONE', value: 'one' }, { name: 'TWO', value: 'two' }]}
-                    renderOptionsBy="name" />
-        </div>
-    </ThemeProvider>
-));
+});
